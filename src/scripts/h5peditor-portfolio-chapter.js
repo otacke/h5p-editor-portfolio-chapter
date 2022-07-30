d199b1ea1bdf90c9b694d3dccea4371d879e613a
Oliver Tacke <o.tacke@posteo.de>
2022-07-30 14:55:10 +0200

    Add initial version

---
+++ src/scripts/h5peditor-portfolio-chapter.js
@@ -0,0 +1,75 @@
+/** Class for Boilerplate H5P widget */
+export default class PortfolioChapter {
+
+  /**
+   * @constructor
+   * @param {object} parent Parent element in semantics.
+   * @param {object} field Semantics field properties.
+   * @param {object} params Parameters entered in editor form.
+   * @param {function} setValue Callback to set parameters.
+   */
+  constructor(parent, field, params, setValue) {
+    this.parent = parent;
+    this.field = field;
+    this.params = params;
+    this.setValue = setValue;
+
+    // Callbacks to call when parameters change
+    this.changes = [];
+
+    // Let parent handle ready callbacks of children
+    this.passReadies = true;
+
+    // DOM
+    this.$container = H5P.jQuery('<div>', {
+      class: 'h5peditor-portfolio-chapter'
+    });
+
+    // Instantiate original field (or create your own and call setValue)
+    this.fieldInstance = new H5PEditor.widgets[this.field.type](this.parent, this.field, this.params, this.setValue);
+    this.fieldInstance.appendTo(this.$container);
+
+    // Relay changes
+    if (this.fieldInstance.changes) {
+      this.fieldInstance.changes.push(() => {
+        this.handleFieldChange();
+      });
+    }
+
+    // Errors (or add your own)
+    this.$errors = this.$container.find('.h5p-errors');
+  }
+
+  /**
+   * Append field to wrapper. Invoked by H5P core.
+   * @param {H5P.jQuery} $wrapper Wrapper.
+   */
+  appendTo($wrapper) {
+    this.$container.appendTo($wrapper);
+  }
+
+  /**
+   * Validate current values. Invoked by H5P core.
+   * @return {boolean} True, if current value is valid, else false.
+   */
+  validate() {
+    return this.fieldInstance.validate();
+  }
+
+  /**
+   * Remove self. Invoked by H5P core.
+   */
+  remove() {
+    this.$container.remove();
+  }
+
+  /**
+   * Handle change of field.
+   */
+  handleFieldChange() {
+    this.params = this.fieldInstance.params;
+    this.changes.forEach(change => {
+      change(this.params);
+    });
+  }
+}
